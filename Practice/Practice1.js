931edd0847966180e2806577be69a70463b90d42
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-06-27 08:28:11 +0530

    27-06-2024

---
+++ Practice/Practice1.js
@@ -0,0 +1,94 @@
+// console.log('Hello JavaScript');
+// console.warn('Warning');
+// document.write('Hello World!');
+// DOM (Document Object Model)
+
+// document.getElementById('demo').innerHTML = "Hello Html!"
+
+// alert('Hi, I am alert')
+
+/*Javascript Basic Concept*/
+
+// Es5 var
+
+// Es6 let const
+
+{
+    // var x = 20
+    // x = 30
+    // var x = 'Hello'
+    // console.log(x);
+}
+
+{
+    // var x = 20;var y = 50; var z = 60
+    // console.log(x);
+    // console.log(y);
+    // console.log(z);   
+}
+
+/*JAVASCRIPT COMMENT*/
+/*Javascript var , let and const */
+
+/*Javascript is case sensitive language */
+
+// var keyword (globaly variable)
+
+// {
+//     var a = 'true'
+//     var b = 'Two'
+//     var c = 'Threee'
+
+//     console.log(a + a);
+//     console.log(b + a + c);
+//     console.log(c + b +b);
+// }
+
+// {
+//     var a = true
+//     var b = false
+//     var c = false
+
+//     console.log(a + a);
+//     console.log(b + a + c);
+//     console.log(c + b + b);
+// }
+
+// Let Keyword (block-scope variable)
+
+// let a = 10
+// a = 50
+// let a = 20 (Not valid syntax)
+// {
+//     console.log(a);
+// }
+
+// Const Keyword (block-scope variable)
+
+// const names = 'Dankhara'
+// {
+//     let names = 'Divyesh'
+//     console.log(names);
+// }
+// {
+//     let names = 'Divyesh'
+//     console.log(names);
+// }
+
+// let names = "skillqode"
+// console.log(names);
+
+// let name_2 = "skillqode"
+// console.log(names);
+
+// let firstName = "skillqode"
+// console.log(names);
+
+// let $name = "skillqode"
+// console.log($name);
+
+// let _names = "skillqode"
+// console.log(_names);
+
+// let 12_names = "skillqode" (Not valid)
+// console.log(12_names);
