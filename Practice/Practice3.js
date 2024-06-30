457813264ac1391e218ce9630fe137973918f5d1
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-06-30 22:11:43 +0530

    30-06-2024

---
+++ Practice/Practice3.js
@@ -0,0 +1,124 @@
+/* Javascript Operators */
+
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Logical operators
+// Bitwise operators
+// String operators
+// Miscellaneous operators
+
+/* Arithmetic Operators / Assignment Operators */
+
+// Addition
+{
+    // let x = 10
+    // let y = 20
+    // x += y
+    // let z = x + y
+    // console.log(z);
+}
+
+// Substraction
+{
+    // let x = -20
+    // let y = 20
+    // let z = 20 + (x - y)
+    // x -= y - z
+    // console.log(z);
+    // console.log(x);
+}
+
+// Multi
+{
+    // let x = 5
+    // let y = 4
+    // let z = x * y 
+    // y *= x
+    // console.log(z);
+}
+
+// Division
+{
+    // let x = 5
+    // let y = 5
+    // let z = x / y
+    // z /= (x += y)
+    // z * x + y
+    // console.log(z);
+    // console.log(x);
+}
+
+// Modulus
+{
+    // let x = 10;
+    // let y = 2;
+    // let z1 = x % y
+    // let z2 = y % x
+    // x %= y
+    // console.log(z1);
+    // console.log(z2);
+}
+
+// Expontiations
+{
+//     let x = 2
+//     let y = 5
+//     let z = x ** y // 5X5
+//     x **= y
+//     console.log(z);
+//     console.log(y);
+}
+
+// Comparision Operators
+
+// Equal
+{
+    // let x = 'Hello'
+    // let y = 'Javascript'
+    // x += y
+    // console.log(x);
+}
+
+// Equal To
+// Not Equal To
+{
+    // let x = 20;
+    // let y = '20'
+    // let z1 = x == y
+    // let z2 = x != y
+    // console.log(z1);
+    // console.log(z2);
+}
+
+// Strict Equality Opera
+// Not Strict Equality Opera
+
+{
+    // let x = '10'
+    // let y = '20'
+    // let z1 = x === y
+    // let z2 = x != y
+    // console.log(z1); 
+    // console.log(z2); 
+}
+
+// less than or Equal To
+{
+    // let x = 10
+    // let y = 10
+    // let z1 = x < y
+    // let z2 = !(x <= y)
+    // console.log(z1);
+    // console.log(z2);
+}
+
+// Gretherthan or Equal To
+{
+    let x = 20;
+    let y = "20";
+    let z1 = x > y
+    let z2 = x >= y
+    console.log(z1);
+    console.log(z2);
+}
