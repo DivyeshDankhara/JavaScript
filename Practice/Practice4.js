c9351e203dd8d54a49b8fe51670519c3d5be0a50
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-07-01 09:00:13 +0530

    01-07-2024

---
+++ Practice/Practice4.js
@@ -0,0 +1,110 @@
+// Javascript Operators
+
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Logical operators
+// Bitwise operators
+// Miscellaneous operators
+// String operators
+
+// Miscellaneous Operators
+
+// Trenary Operators
+
+// ? :
+{
+    // let Age = 18
+
+    // let result = Age >= 18 ? 'Adult' : 'Child'
+
+    // console.log(result);
+}
+
+// ,
+{
+    // let x = (10 , 20 , 30)
+    // console.log(x);
+    // a = 20 , b = 30 , c = 40
+
+    // a = 20;
+    // b = 30;
+    // c = 40;
+}
+
+// type of
+{
+    // let x = 20;
+    // let y = 10000n
+    // let z = null
+
+    // console.log(typeof x);
+    // console.log(typeof y);
+    // console.log(typeof z);
+}
+
+// BitWise Operators
+
+// BitWise And()
+// When a bitwise AND is performed on a pair of bits, it returns 1 if both bits are 1.
+{
+    // let x = 1
+    // let y = 0
+    // console.log(x & y);
+    // console.log(y & x);
+    // console.log(x & x);
+    // console.log(y & y);
+} 
+
+// Bitwise origin()
+// When a bitwise Or is performed on a pair of bits, it return 1 if one of the bits is 1.
+{
+    // let x = 1
+    // let y = 0
+    // console.log(x | y);
+    // console.log(y | x);
+    // console.log(x | x);
+    // console.log(y | y);
+}
+
+// Bitwise XOR()
+// When a bitwise XOR is performed on a pair of bits, it returns 1 if the bits are different.
+{
+    // let x = 1
+    // let y = 0
+    // console.log(x ^ y);
+    // console.log(y ^ x);
+    // console.log(x ^ x);
+    // console.log(y ^ y);
+}
+
+// Logical Operator
+
+// Logical And
+{
+    // let x = false
+    // let y = false
+    // let z = x & y
+
+    // console.log(z);
+}
+
+// Logical OR
+{
+    // let x = true
+    // let y = true
+    // let z = x || y
+
+    // console.log(z);
+}
+
+// Logical NOT
+{
+    let x = true
+    let y = false
+    let z = !(y)
+
+    let zz = !(true && false) + (false && true)
+
+    console.log(zz);
+}
