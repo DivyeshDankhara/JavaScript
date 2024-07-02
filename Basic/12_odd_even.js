d4a8e1b4d63eeefbf66501d899162283e19395a2
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-07-02 10:03:54 +0530

    02-07-2024

---
+++ Basic/12_odd_even.js
@@ -0,0 +1,7 @@
+let n = prompt("Enter the number you check odd or even :-> ")
+
+if(n%2==0){
+    console.log(`${n} is even`);
+}else{
+    console.log(`${n} is odd`);
+}
---
+++ Basic/13_large.js
@@ -0,0 +1,8 @@
+let n1 = prompt("Enter the number1 :-> ")
+let n2 = prompt("Enter the number2 :-> ")
+
+if(n1>n2){
+    console.log(`${n1} is large`);
+}else{
+    console.log(`${n2} is large`);
+}
