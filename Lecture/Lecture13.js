dce747bb0fa6d2108866cc2add296fd008cae1ed
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-07-11 11:34:34 +0530

    11-07-2024

---
+++ Lecture/Lecture13.js
@@ -0,0 +1,51 @@
+// Function
+
+// add();
+
+// function add()
+// {
+//     console.log("Hello");
+// }
+
+
+// function sum(a,b){
+//     return a + b;
+// }
+// function sum(a,b){
+//     console.log("Total : ",a + b);
+// }
+
+// sum("a",10)
+// let c = sum(10,12.8);
+// console.log(c);
+
+
+// Constructor
+
+// let data = new Function("a","b",'return (a*b)');
+// console.log(data(10,20));
+
+
+// IIFE (Immediately Invoked Function Expression)
+
+// (function hello(a,b){
+//     console.log(a*b);
+// })(11,5);
+
+
+// Functon as avariable  (anonymous function)
+
+// let data = function (a,b) {
+//     console.log("sum is :- ", a + b);
+//     return a*b
+// }
+// console.log(data(10,20));
+
+
+// Arrow Function
+
+// let hello = (a,b,c) => a*b*c
+// console.log(hello(10,20,30));
+
+// let hello = (a,b,c) => { return  a*b*c}
+// console.log(hello(10,20,30));
