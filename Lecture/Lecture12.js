510f78e265199c12b6e148eeeb0e282aa28e276b
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-07-11 10:08:04 +0530

    11-07-2024

---
+++ Lecture/Lecture12.js
@@ -0,0 +1,50 @@
+// date Methods
+
+// let date = new Date();
+// let date = new Date("2004-12-19 6:30");
+// let date = new Date(2020, 15, 20, 12, 30, 70, 1000);
+// let date = new Date(123456789);
+// console.log(date);
+
+
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+// console.log(date.toUTCString());
+// console.log(date.toISOString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+
+
+// get & set
+
+let date = new Date();
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getTime());
+
+
+// date.setFullYear(2033)
+// console.log(date);
+
+// date.setMonth(2)
+// console.log(date);
+
+// date.setDate(14)
+// console.log(date);
+
+// date.setHours(14)
+// console.log(date);
+
+// date.setSeconds(56);
+// console.log(date);
+
+// date.setMilliseconds(12);
+// console.log(date);
+
+// console.log(date.getTimezoneOffset());
